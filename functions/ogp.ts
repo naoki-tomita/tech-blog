184fed62d070a52bd7836060a7665f961e3fb799
Naoki Tomita🐱 <mxcpy608@hotmail.com>
2023-05-31 22:45:15 +0900

    improve image

--- functions/ogp.ts
+++ functions/ogp.ts
@@ -4,9 +4,9 @@ import type { Handler } from "@netlify/functions";
 export const handler: Handler = async (event, context) => {
   try {
     const id = event.path.split("/").filter(Boolean)[3];
-    const canvas = createCanvas(600, 400);
+    const canvas = createCanvas(600, 320);
     const context = canvas.getContext("2d");
-    context.fillStyle = "#cccccc";
+    context.fillStyle = "#444444";
     context.font = "30px Impact";
     context.fillText(`Tech blog #${id}`, 40, 40);
     const stream = canvas.toBuffer("image/png");
