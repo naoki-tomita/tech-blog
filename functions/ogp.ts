dd43cef8623ca289fdecbe0f4f0cab0088689c36
Naoki Tomita🐱 <mxcpy608@hotmail.com>
2023-05-31 22:12:16 +0900

    add log

--- functions/ogp.ts
+++ functions/ogp.ts
@@ -2,7 +2,7 @@ import type { HandlerEvent, HandlerContext, Handler } from "@netlify/functions";
 
 export const handler: Handler = async (event, context) => {
   try {
-
+    console.log(JSON.stringify(event));
     return { statusCode: 200, body: JSON.stringify({ foo: "bar" }) };
   } catch (error) {
     console.log(error);
