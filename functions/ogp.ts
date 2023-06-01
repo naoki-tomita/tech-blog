98c9f94d5b1d16f0cfaca301ba3527f06edaf8bf
Naoki Tomita🐱 <mxcpy608@hotmail.com>
2023-06-01 22:54:59 +0900

    fix dirname

--- functions/ogp.ts
+++ functions/ogp.ts
@@ -6,7 +6,7 @@ export const handler: Handler = async (event, context) => {
   try {
     const id = event.path.split("/").filter(Boolean)[3].replace(".jpg", "");
     const response: any = await fetch(`https://ku-tech.microcms.io/api/v1/blogs/${id}`, { headers: { "X-MICROCMS-API-KEY": "XZFkLvLrr209UvvuQBAUH4RxR6SBIVIUo2pq" } }).then(it => it.json());
-    registerFont(__dirname + "./font.ttf", { family: "Rampart One" });
+    registerFont(__dirname + "/font.ttf", { family: "Rampart One" });
     const canvas = createCanvas(600, 320);
     const context = canvas.getContext("2d");
     context.fillStyle = "#444444";
