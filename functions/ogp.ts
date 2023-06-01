ec4e07d4b20b7b74a0abff3bed0f8b4ca202b31b
Naoki Tomita🐱 <mxcpy608@hotmail.com>
2023-06-01 23:15:28 +0900

    Fix extension

--- functions/ogp.ts
+++ functions/ogp.ts
@@ -9,7 +9,7 @@ const Height = 320;
 
 export const handler: Handler = async (event, context) => {
   try {
-    const id = event.path.split("/").filter(Boolean)[3].replace(".jpg", "");
+    const id = event.path.split("/").filter(Boolean)[3].replace(".png", "");
     const response: any = await fetch(`https://ku-tech.microcms.io/api/v1/blogs/${id}`, { headers: { "X-MICROCMS-API-KEY": "XZFkLvLrr209UvvuQBAUH4RxR6SBIVIUo2pq" } }).then(it => it.json());
     registerFont(__dirname + "/font.ttf", { family: "Rampart One" });
     const canvas = createCanvas(Width, Height);
