e121d596b5c5c8e4ffc555595373013193fcc127
Naoki Tomita🐱 <mxcpy608@hotmail.com>
2023-06-01 23:09:17 +0900

    Render background

--- functions/ogp.ts
+++ functions/ogp.ts
@@ -2,18 +2,27 @@ import { createCanvas, registerFont } from "canvas";
 import type { Handler } from "@netlify/functions";
 import fetch from "node-fetch";
 
+const BackgroundColor = "#18232c";
+const FontColor = "#edf0f3";
+const Width = 800;
+const Height = 320;
+
 export const handler: Handler = async (event, context) => {
   try {
     const id = event.path.split("/").filter(Boolean)[3].replace(".jpg", "");
     const response: any = await fetch(`https://ku-tech.microcms.io/api/v1/blogs/${id}`, { headers: { "X-MICROCMS-API-KEY": "XZFkLvLrr209UvvuQBAUH4RxR6SBIVIUo2pq" } }).then(it => it.json());
     registerFont(__dirname + "/font.ttf", { family: "Rampart One" });
-    const canvas = createCanvas(600, 320);
+    const canvas = createCanvas(Width, Height);
     const context = canvas.getContext("2d");
-    context.fillStyle = "#fff";
-    context.font = `20px "Impact"`;
+    context.fillStyle = BackgroundColor;
+    context.fillRect(0, 0, Width, Height);
+
+    context.fillStyle = FontColor;
+    context.font = `30px "Impact"`;
     context.fillText(`Tech blog`, 40, 40);
-    context.font = `30px "Rampart One"`;
+    context.font = `40px "Rampart One"`;
     context.fillText(response.title, 40, 80);
+
     const stream = canvas.toBuffer("image/jpeg");
 
     return {
