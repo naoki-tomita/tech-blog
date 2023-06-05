2c9cd7a4ffefbd9585a72898bad6e3e050d5bcc3
Naoki Tomita🐱 <mxcpy608@hotmail.com>
2023-06-05 10:57:40 +0900

    File splitting

--- functions/rss.ts
+++ functions/rss.ts
@@ -1,30 +1,5 @@
 import { h, renderToText } from "zheleznaya";
-
-const Entry = ({ id, title, updatedAt, summary }) => {
-  return (
-    <entry>
-      <id>https://ku-tech.netlify.app/{id}</id>
-      <title>{title}</title>
-      <link rel="alternate" type="text/html" href={`https://ku-tech.netlify.app/${id}`} />
-      <updated>{updatedAt}</updated>
-      <summary>{summary}</summary>
-    </entry>
-  );
-}
-
-const Rss = ({ entries }) => {
-  return (
-    <feed xmlns="http://www.w3.org/2005/Atom" xml:lang="ja">
-      <id>tag:ku-tech.blog/</id>
-      <title>Tech blog</title>
-      <updated>{new Date().toISOString()}</updated>
-      <link rel="alternate" type="text/html" href="http://ku-tech.netlify.app/feed/" />
-      <link rel="self" type="application/atom+xml" href="https://ku-tech.netlify.app/.netlify/functions/rss" />
-      {entries.map(it => <Entry {...it} />)}
-    </feed>
-  );
-}
-
+import { Rss } from "./RssComponent";
 
 export const handler = async () => {
   try {
@@ -32,7 +7,7 @@ export const handler = async () => {
     return {
       statusCode: 200,
       headers: { "content-type": "text/html" },
-      body: renderToText(<Rss entries={entries}/>)
+      body: renderToText(h(Rss, { entries })),
     };
   } catch (error) {
     console.log(error);
