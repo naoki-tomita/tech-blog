ccbaedee12740e5f4d352967cc4115831a688115
Naoki Tomita🐱 <mxcpy608@hotmail.com>
2023-07-27 20:15:52 +0900

    Add table of contents.

--- src/Store.ts
+++ src/Store.ts
@@ -2,6 +2,13 @@ import { createStore } from "zheleznaya";
 import { client } from "./MicroCmsClient";
 import { createRouter } from "@kojiro.ueda/zrouter";
 
+export type TOC = {
+  tagName: string;
+  text: string;
+  id: string;
+  children: TOC[];
+}
+
 export const store = createStore<{
   path: string;
   articles: {
@@ -18,6 +25,7 @@ export const store = createStore<{
       content: string;
       publishedAt: string;
     } | null;
+    tableOfContents: TOC[];
   };
 }>({
   path: location.pathname,
@@ -27,6 +35,7 @@ export const store = createStore<{
   article: {
     id: null,
     content: null,
+    tableOfContents: [],
   }
 });
 
@@ -36,14 +45,45 @@ export async function loadList() {
   store.articles.list = list.contents;
 }
 
+const headingTags = ["h1", "h2", "h3"];
+function createTableOfContents(html: string) {
+  const div = document.createElement("div");
+  div.innerHTML = html;
+  const headings = [...div.childNodes]
+    .filter(it => headingTags.includes(it.nodeName.toLowerCase()))
+    .map(it => it as HTMLElement)
+    .map(it => ({ tagName: it.nodeName.toLowerCase(), text: it.textContent!, children: [], id: it.id }))
+  const toc: TOC[] = []
+  headings.forEach(it => {
+    if (it.tagName === "h1" || toc.length === 0) {
+      toc.push(it);
+      return;
+    }
+    const children = toc[toc.length - 1].children;
+    if (it.tagName === "h2" || children.length === 0) {
+      children.push(it);
+      return;
+    }
+    const grandChildren = children[children.length - 1].children;
+    if (it.tagName === "h3") {
+      grandChildren.push(it)
+      return;
+    }
+  });
+  return toc;
+}
+
 export async function loadItem(id: string) {
   store.article.content = null;
   store.article.id = null;
+  store.article.tableOfContents = [];
 
   const article = await client.item(id);
+  const tableOfContents = createTableOfContents(article.content);
 
   store.article.id = id;
   store.article.content = article;
+  store.article.tableOfContents = tableOfContents;
 
   // for netlify.
   setTimeout(() => {(window as any).prerenderReady = true;}, 100);
--- src/pages/Article.tsx
+++ src/pages/Article.tsx
@@ -1,19 +1,57 @@
 import { Component, h, createEffect } from "zheleznaya";
-import { loadItem, store } from "../Store";
+import { css } from "zstyl";
+import { TOC, loadItem, store } from "../Store";
 import { Title } from "../components/Title";
 import { Html } from "../components/Html";
+
+
+const TableOfContent: Component<{ toc: TOC }> = ({ toc }) => {
+  return (
+    <li class={css`
+      font-size: 12px;
+    `}>
+      <a href={`#${toc.id}`}>{toc.text}</a>
+      {toc.children.length > 0 && <TableOfContents tocs={toc.children} /> || ""}
+    </li>
+  );
+
+}
+const TableOfContents: Component<{ tocs: TOC[] }> = ({ tocs }) => {
+  return (
+    <ul>
+      {tocs.map(toc => <TableOfContent toc={toc}/>)}
+    </ul>
+  );
+}
+
 
 const effect = createEffect();
 export const ArticlePage: Component<{ id: string }> = ({ id }) => {
   effect(() => loadItem(id), [id]);
 
   return (
-    <article>
-      <Title label={store.article.content?.title!} />
-      <header>
-        <h1>{store.article.content?.title}</h1>
-      </header>
-      <Html html={store.article.content?.content}/>
-    </article>
+    <div class={css`
+      display: flex;
+      align-items: flex-start;
+      gap: 8px;
+    `}>
+      <article class={css`
+        flex: 10;
+        min-width: 0;
+      `}>
+        <Title label={store.article.content?.title!} />
+        <header>
+          <h1>{store.article.content?.title}</h1>
+        </header>
+        <Html html={store.article.content?.content}/>
+      </article>
+      <article class={css`
+        flex: 3;
+        min-width: 0;
+        padding: 16px 16px;
+      `}>
+        <TableOfContents tocs={store.article.tableOfContents} />
+      </article>
+    </div>
   );
 }
